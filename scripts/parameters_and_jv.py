3e5f11e29da1889f535c75c1114fc30443020991
jjia <972931271@qq.com>
2021-10-06 19:56:02 +0200

    Add main code

--- scripts/parameters_and_jv.py
+++ scripts/parameters_and_jv.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     device_ids = ['4']
 
     abs_dir_path = os.path.dirname(os.path.realpath(__file__))  # abosolute path of the current .py file
-    source_dir = os.path.join(abs_dir_path, "../socsa/data", "4")
+    source_dir = os.path.join(abs_dir_path, "data", "4")
     target_dir = os.path.join(source_dir, "after_processing")
     if not os.path.isdir(target_dir):
         os.makedirs(target_dir)
