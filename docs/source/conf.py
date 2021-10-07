16211a0022f3872ec3b695f6a76c49da8cfd7717
jjia <972931271@qq.com>
2021-10-07 22:01:55 +0200

    update PYTHONPATH

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,8 +12,11 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../..'))
-sys.path.insert(0, os.path.abspath('../../socsa'))
+# sys.path.insert(0, os.path.abspath('../..'))
+# sys.path.insert(0, os.path.abspath('../../socsa'))
+sys.path.insert(0, os.path.abspath(".."))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..")))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..", 'socsa')))
 
 # -- Project information -----------------------------------------------------
 
